1ef679adc3a15222b2e075ebcc5fb77ff54bd522
Oleg Shamaev <oleg.shamaev.1993@gmail.com>
2026-07-18 21:40:40 +0300

    fix error in terraform-apply.yml and append outputs in module03/oidc

--- module03/oidc/outputs.tf
+++ module03/oidc/outputs.tf
@@ -1,4 +1,9 @@
 output "github_actions_role_arn" {
   value       = aws_iam_role.github_iam_role_readonly.arn
-  description = "ARN of the role assumed by GitHub Actions via OIDC"
+  description = "ARN of the role assumed by GitHub Actions via OIDC for only tf plan"
+}
+
+output "github_actions_role_apply_arn" {
+  value       = aws_iam_role.github_iam_role_apply.arn
+  description = "ARN of the role assumed by GitHub Actions via OIDC for tf apply"
 }
