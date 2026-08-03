7ec6edc070c4ff3340350df5c63beb94d77b1f79
Oleg Shamaev <oleg.shamaev.1993@gmail.com>
2026-08-03 22:46:06 +0300

    add repo for frontend in ecr

--- module04/ecr/main.tf
+++ module04/ecr/main.tf
@@ -1,6 +1,7 @@
 locals {
   repositories = {
     backend = { name = "website/backend", scan_on_push = true, mutability = "IMMUTABLE" }
+    frontend = { name = "website/frontend", scan_on_push = true, mutability = "IMMUTABLE" }
     demo    = { name = "website/demo", scan_on_push = false, mutability = "MUTABLE" }
   }
 }
